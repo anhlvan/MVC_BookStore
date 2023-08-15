9ee9bc034a81486807fd27495f89701df70f6aa0
anhlvan <dunyc0110@gmail.com>
2023-08-16 00:38:28 +0700

    update layout

--- App/Views/Shared/_LayoutAdmin.php
+++ App/Views/Shared/_LayoutAdmin.php
@@ -80,11 +80,11 @@
                         <div class="col-sm-6">
                             <script>
                                 document.write(new Date().getFullYear())
-                            </script> © SenMS.
+                            </script>
                         </div>
                         <div class="col-sm-6">
                             <div class="text-sm-end d-none d-sm-block">
-                                Design & Develop by <a href="/" class="text-decoration-underline">HMZTeam</a>
+                                Design & Develop by <a href="/" class="text-decoration-underline">Nhóm 1</a>
                             </div>
                         </div>
                     </div>
